635ea9e7e1566683be06d56aafc0ebb74f55d525
reallykyuminp <kyuminp@andrew.cmu.edu>
2025-01-25 04:09:41 -0500

    Pull Firebase

--- patient-app/src/App.js
+++ patient-app/src/App.js
@@ -4,7 +4,10 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage";
 import PatientPage from "./PatientPage";
 import TextPage from "./TextPage"; 
+import PatientInfo from "./PatientInfo"; // Import PatientInfo component
 import { getPatients, getNotes, updateNote } from "./dataService"; // Import data functions
+import { db } from "./firebaseConfig"; // Import Firestore database
+import { collection, getDocs } from "firebase/firestore"; // Import Firestore functions
 
 function App() {
   const [patients, setPatients] = useState({});
@@ -14,7 +17,18 @@ function App() {
   useEffect(() => {
     getPatients().then(setPatients);
     getNotes().then(setNotes);
+    fetchAllPatients(); // Fetch all patients
   }, []);
+
+  // Fetch all patients from Firestore
+  const fetchAllPatients = async () => {
+    const querySnapshot = await getDocs(collection(db, "patient_info"));
+    const patients = [];
+    querySnapshot.forEach((doc) => {
+      patients.push({ id: doc.id, ...doc.data() });
+    });
+    console.log(patients); // List of all patients
+  };
 
   return (
     <Router>
@@ -22,6 +36,7 @@ function App() {
         <Route path="/" element={<HomePage patients={patients} />} />
         <Route path="/patient/:id" element={<PatientPage patients={patients} setPatients={setPatients} notes={notes} />} />
         <Route path="/text/:id/:noteId" element={<TextPage notes={notes} updateText={(id, noteId, text) => updateNote(patients, setNotes, id, noteId, text)} />} />
+        <Route path="/patient-info" element={<PatientInfo />} /> {/* Add route for PatientInfo */}
       </Routes>
     </Router>
   );
---
+++ patient-app/src/firebaseConfig.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCa6AqyA-sPiHV5xSbvp72nty0XnUH1OLc",
+    authDomain: "hoyahacks-54bf3.firebaseapp.com",
+    projectId: "hoyahacks-54bf3",
+    storageBucket: "hoyahacks-54bf3.firebasestorage.app",
+    messagingSenderId: "484579306298",
+    appId: "1:484579306298:web:d0b0f3355450cb90387e03",
+    measurementId: "G-08VGM953H3"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = getFirestore(app);
+
+export { db };
